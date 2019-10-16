86cba45ed3a0136debd995f2428cc8f78a19427f
Nick Panagakis <npanagakis101@gmail.com>
2019-10-15 19:38:14 -0500

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,15 +21,6 @@ function preload (){
     //this.load.setBaseURL('https://labs.phaser.io');
     this.load.image('ship', 'assets/images/ship.png');
     this.load.image('asteroid','assets/images/pixel_asteroid.png');
-
-    //this.load.setBaseURL('https://labs.phaser.io');
-    this.load.image('ship', 'assets/images/ship.png');
-    this.load.image('asteroid','assets/images/pixel_asteroid.png');
-    this.load.setBaseURL('https://labs.phaser.io');
-
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
 }
 
 function create (){
