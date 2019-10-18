77929c0909e618447acc56a09fafe3816f01ea10
Dominique McLean <dommclean13@gmail.com>
2019-10-17 21:22:14 -0500

    api edit

--- assets/javascript/firebase-soundcloud.js
+++ assets/javascript/firebase-soundcloud.js
@@ -1,19 +1,52 @@
 // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyClwRXheZqZBWVw78UKc_agW1OJekjqSw8",
-    authDomain: "the-game-63f83.firebaseapp.com",
-    databaseURL: "https://the-game-63f83.firebaseio.com",
-    projectId: "the-game-63f83",
-    storageBucket: "the-game-63f83.appspot.com",
-    messagingSenderId: "183555544943",
-    appId: "1:183555544943:web:4730fb7de6051856aafa9a",
-    measurementId: "G-WD2PVF9L1J"
-  };
+  // var firebaseConfig = {
+  //   apiKey: "AIzaSyClwRXheZqZBWVw78UKc_agW1OJekjqSw8",
+  //   authDomain: "the-game-63f83.firebaseapp.com",
+  //   databaseURL: "https://the-game-63f83.firebaseio.com",
+  //   projectId: "the-game-63f83",
+  //   storageBucket: "the-game-63f83.appspot.com",
+  //   messagingSenderId: "183555544943",
+  //   appId: "1:183555544943:web:4730fb7de6051856aafa9a",
+  //   measurementId: "G-WD2PVF9L1J"
+  // };
 
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.database();
+  // // Initialize Firebase
+  // firebase.initializeApp(firebaseConfig);
+  // firebase.database();
+
+
+
+
+  $(".click").on("click", function() {
+
+    var sad = "lose+game&safesearch=true&image_type=illustration&category=sad"
+
+    var queryURL = "https://pixabay.com/api/?key=13979984-d2ae798e35c78a69340f780d3&q=" + sad ;
+
+
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+    
+  }).then(function(response) {
+    console.log(response);
+
+    
+    var image = $("<img>").addClass("img-fluid").attr("src", response.hits[0].previewURL);
+
+    $("#poster").empty().append(image);
+
+
+  });
+  
+  
+  
+});
+
+
+// url: "https://pixabay.com/api/?key=13979984-d2ae798e35c78a69340f780d3&q=lose+game&safesearch=true&image_type=illustration&category=sad",
 
 
 
