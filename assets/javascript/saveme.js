a0f75ccd0b1136e6b104cd2598cda9b1f6c86838
Nick <nick@panagakis.com>
2019-10-17 21:17:32 -0500

    asdf

---
+++ assets/javascript/saveme.js
@@ -0,0 +1,10 @@
+// exhaust particle progress
+//var playerExhaust = this.add.particles('exhaust');
+//var playerEmitter = playerExhaust.createEmitter({
+//    speed: 3,
+//    scale: { start: 1, end: 0 },
+//    blendMode: 'ADD',
+//    maxParticles: 5,
+//    accelerationX: 300        
+//});
+//playerEmitter.startFollow(player);
